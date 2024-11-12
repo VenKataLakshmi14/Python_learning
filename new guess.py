d4f1b357e987ed3149de03ff9f3aa5a54a08d447
Venkatalakshmi14 <saransivakumar104@gmail.com>
2024-11-12 17:23:48 +0530

    Guess  Game

--- new guess.py
+++ new guess.py
@@ -1,15 +1,12 @@
 import random
-
 secret_number = random.randint(1, 10)
 guess_limit = 3
-
 for guess_count in range(guess_limit):
     try:
         guess = int(input(f"Guess {guess_count + 1}: "))
     except ValueError:
         print("Invalid input.")
         continue
-
     if guess == secret_number:
         print("You won")
         break
